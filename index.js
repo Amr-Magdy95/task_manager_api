3a6e4b6703d492a77df06325b97d8e77ab11df5e
Amr-Magdy95 <37376273+Amr-Magdy95@users.noreply.github.com>
2023-10-04 17:07:55 +0300

    Feature: Added mongoose connection to the DB

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+require("dotenv").config();
+const connectDB = require("./db/connect");
 
 // middleware
 app.use(express.json());
@@ -10,6 +12,15 @@ app.get("/", (req, res) => {
 });
 app.use("/api/v1/tasks", require("./routes/tasks"));
 
-app.listen(port, () => {
-  console.log(`server is now listening on port ${port}...`);
-});
+const start = async () => {
+  try {
+    await connectDB();
+    console.log("Connect to the DB");
+    app.listen(port, () => {
+      console.log(`server is now listening on port ${port}...`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+start();
