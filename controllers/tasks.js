c18b22009827e7748e9e914a693ab267c918dfad
Amr-Magdy95 <37376273+Amr-Magdy95@users.noreply.github.com>
2023-10-05 10:04:35 +0300

    Feature: added basic CRUD for tasks controllers

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,19 +1,52 @@
-exports.getAllTasks = (req, res) => {
-  res.send("all tasks");
+const Task = require("../models/Task");
+
+exports.getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find();
+    res.status(200).json({ tasks });
+  } catch (err) {
+    res.status(500).json({ msg: err });
+  }
 };
 
-exports.createTask = (req, res) => {
-  res.json(req.body);
+exports.createTask = async (req, res) => {
+  try {
+    const tasks = await Task.create(req.body);
+    res.status(201).json({ tasks });
+  } catch (err) {
+    res.status(500).json({ msg: err });
+  }
 };
 
-exports.getTask = (req, res) => {
-  res.json(req.params.id);
+exports.getTask = async (req, res) => {
+  try {
+    const task = await Task.findOne({ _id: req.params.id }).exec();
+    if (!task) return res.status(404).json({ msg: "could not find user" });
+    res.status(200).json(task);
+  } catch (err) {
+    res.status(500).json({ msg: err });
+  }
 };
 
-exports.updateTask = (req, res) => {
-  res.json(req.params.id);
+exports.deleteTask = async (req, res) => {
+  try {
+    const task = await Task.findOneAndDelete({ _id: req.params.id });
+    if (!task) return res.status(404).json({ msg: "task not found" });
+    res.status(200).json({ task });
+  } catch (err) {
+    res.status(500).json({ msg: err });
+  }
 };
 
-exports.deleteTask = (req, res) => {
-  res.json(req.params.id);
+exports.updateTask = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const task = await Task.findOneAndUpdate({ _id: id }, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!task) return res.status(404).json({ msg: "task not found" });
+
+    res.status(200).json({ task });
+  } catch (err) {}
 };
--- index.js
+++ index.js
@@ -5,11 +5,10 @@ require("dotenv").config();
 const connectDB = require("./db/connect");
 
 // middleware
+app.use(express.static("./public"));
 app.use(express.json());
 // routes
-app.get("/", (req, res) => {
-  res.status(200).send("<h1>Hello</h1>");
-});
+
 app.use("/api/v1/tasks", require("./routes/tasks"));
 
 const start = async () => {
